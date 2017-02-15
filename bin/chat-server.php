d6be0f108d3b98a92f735be050803d2e0da52a56
Ashraf <rootkali95@gmail.com>
2017-02-15 16:59:29 +0800

    pretty code

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -7,5 +7,10 @@ use Ratchet\Http\HttpServer;
 require __DIR__ . '/../vendor/autoload.php';
 
 
-$server = IoServer::factory(new HttpServer(new WsServer(new Chat())), 8080);
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(new Chat())
+    ),
+    8080);
+
 $server->run();
